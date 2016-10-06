2cca076a1cc24294e6922870a8607c67371f5b02
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2016-10-06 02:14:12 -0600

    Beautify

--- code/TrainTest.py
+++ code/TrainTest.py
@@ -6,11 +6,11 @@ Created on Aug 28, 2016
 import numpy as np
 import time
 
+
 class TrainTest(object):
     '''
     classdocs
     '''
-
 
     def __init__(self, learn, trainX=np.array([[]]), trainY=np.array([]), testX=np.array([[]]), testY=np.array([]), h_param=0):
         self.learn = learn
@@ -19,7 +19,7 @@ class TrainTest(object):
         self.testX = testX
         self.testY = testY
         self.h_param = h_param
-        
+
     def run_tt(self):
         output = {}
         print 'Training...'
@@ -40,7 +40,7 @@ class TrainTest(object):
         Y = np.array(Y)
         sizeY = Y.shape
         if len(sizeY) < 2:
-            Y = np.reshape(Y, (sizeY[0],1))
+            Y = np.reshape(Y, (sizeY[0], 1))
         if(len(Y) == len(self.testY)):
             overlp = [Y == self.testY]
             overlp_sum = sum(sum(overlp))
@@ -48,14 +48,11 @@ class TrainTest(object):
             return output
         print 'cannot determine accuracy'
         return 0
-            
-    def verifyAcc(self,acc, desired):
+
+    def verifyAcc(self, acc, desired):
         print "accuracy is ", acc, "... should be above ", desired, "..."
         if acc >= desired:
             print "passed!"
             return 1
         print "failed"
         return 0
-    
-       
-        
